5b7691e3892dd598823a0d0ba05e3842880fc010
Alexei Ptitchkin <ptitchkin@gmail.com>
2019-02-17 09:40:05 -0500

    neutralize tests by commenting test body

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-/*   const div = document.createElement('div');
+/*   
+ * neutralize tests by commenting test body
+
+
+const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div); */
 });
